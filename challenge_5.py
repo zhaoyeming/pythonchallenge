323cc4865f73c4287c0b85375de584ffa7e47392
Zhao Yeming <zhaoyeming@gmail.com>
2012-08-15 12:05:25 +0800

    Implement using print, join and list comprehension.

--- challenge_5.py
+++ challenge_5.py
@@ -1,12 +1,9 @@
 """Challenge 5 at http://www.pythonchallenge.com/pc/def/peak.html"""
 
 import pickle
-import sys
 
 if __name__ == '__main__':
     with open('challenge_5.dat', 'r') as f:
         data = pickle.load(f)
         for line in data:
-            for c, n in line:
-                sys.stdout.write(c * n)
-            print
+            print ''.join([c * n for c, n in line])
